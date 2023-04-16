83ff2c7ce2ba7565c13d8caab9eadc0ba9a8cdf8
Odelu1315 <odelu423@gmail.com>
2023-04-17 00:57:02 +0530

    Assignment

--- NodeJsClass/firstapp1/src/app/app.module.ts
+++ NodeJsClass/firstapp1/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ServiceBoxComponent } from './service-box/service-box.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     ServiceBoxComponent
-  ],//apecifies includes all the  components ,pipes and directories are part of the module
+  ],// specifies or includes all the components.pipes and directives which
+  //are part of the module
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],//services will specified here
-  bootstrap: [AppComponent]//root components
+  providers: [],//services will be specified here
+  bootstrap: [AppComponent] //root components
 })
 export class AppModule { }
